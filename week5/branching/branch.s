a8cff662f26b7077dbfedfbba3d718f558d094a2
Chris <the_rocket_scientist@icloud.com>
2022-02-02 01:40:39 +0000

    working on branching

---
+++ week5/branching/branch.s
@@ -0,0 +1,18 @@
+.global main
+main:
+    mov r1, #2
+    mov r2, #5
+
+    cmp r1, r2
+    beq case_equal
+
+
+case_different:
+    mov r0, #4
+    b end
+
+case_equal:
+    mov r0, #1
+
+end:
+    bx lr
--- week5/variable/var.s
+++ week5/variable/var.s
@@ -1,22 +1,24 @@
 .data                   @ Data section
-.balign 4               @ align  words into 4 byte alignments
+.balign 4               @ byte alignment (we want word alignment so we put 4)
 
-x:                      @ create a variable called x
-    .word 0             @ the type of the variable x is a word(4bytes) with value 0
+x:                      @ create a variable called x which is an integer
+    .word 0             @ initializing the x variable to 0
 
-y:
-    .word 0
+y:                      @ create a variable y, which is an integer
+    .word 0             @ since it is an integer which is 4 bytes 1 word in size and initialized to 0
 
 .text                   @ Text section
-.global main        
+.global main            @ create a global label called main
 
-main:
+main:                   @ this label refers to an address and it is the start of the first instruction
     mov r1, #13         @ Place 13 into register 1
-    ldr r2, addressOfX  
-    str r1, [r2]
+    ldr r2, addressOfX  @ place the address of the x variable in data section into register r2
+    str r1, [r2]        @ r2 is and address, [r2] is dereferencing and taking the VALUE in r2 and placing it in r1
+                        @ we are storing the number that is in r1 and putting it in an address location
 
-    mov r1, #14
-    ldr r3, addressOfY
+
+    mov r1, #14         @ place 14 in r1, this is overwriting the value currently in r1
+    ldr r3, addressOfY  @ 
     str r1, [r3]
 
     ldr r4, [r2]
@@ -25,5 +27,5 @@ main:
 
     bx lr
 
-addressOfX: .word x
-addressOfY: .word y
+addressOfX: .word x     @ pointers to the address of the x variable in data section
+addressOfY: .word y     @ pointer to the address of y variable in data section
