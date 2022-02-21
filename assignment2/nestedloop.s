715b857b281176097b8da1a1ab39c83f56d408ee
Chris <the_rocket_scientist@icloud.com>
2022-02-21 06:43:51 +0000

    got the square

--- assignment2/nestedloop.s
+++ assignment2/nestedloop.s
@@ -3,12 +3,10 @@
 
 .data
 
-i: .word 0
+i: .word 1
 j: .word 0
-outer_length: .word 7
-inner1_length: .word 8
-inner2_length: .word 8
-base_length: .word 8
+space: .word 0
+rows: .word 8
 
 stars:  .asciz  "*"
 spaces: .asciz  "+"
@@ -16,103 +14,56 @@ newline: .asciz "\n"
 
 
 .text
-main: 
-    str lr, [sp, #-4]!          // push the lr to the stack
-
-outer:
+main:
+    str lr, [sp, #-4]!
+ 
+loop0:
     ldr r1, addressOfI
-    ldr r2, addressOfOuterLength
-    ldr r3, [r1]
-    mov r4, #0
-    str r4, [r1]
-    ldr r4, [r2]
-    b outer_mid
-
-outer_loop:
-    ldr r1, addressOfI
-    ldr r2, addressOfOuterLength
+    ldr r2, addressOfRows
     ldr r3, [r1]
     ldr r4, [r2]
+
     cmp r3, r4
-    bge outer_done
-    
+    bgt done
 
-    ldr r0, addressOfNewline
-    bl printf
-    
-
-inner2:
     ldr r1, addressOfJ
-    ldr r2, addressOfInner2Length
+    ldr r2, [r1]
+    mov r2, #1
+    str r2, [r1]
+
+loop1:
+    ldr r1, addressOfJ
+    ldr r2, addressOfRows
     ldr r3, [r1]
     ldr r4, [r2]
-    add r4, r3, r4
-    sub r4, r4, #1
-    str r4, [r2]
-    b inner2_mid
+    
+    
+    cmp r3, r4
+    bgt loop0_end
 
-inner2_loop:
     ldr r0, addressOfStars
     bl printf
+
+loop1_end:
     ldr r1, addressOfJ
-    ldr r2, addressOfInner2Length
-    ldr r3, [r1]
-    add r3, r3, #1
-    str r3, [r1]
-    ldr r4, [r2]
+    ldr r2, [r1]
+    add r2, r2, #1
+    str r2, [r1]
+    b loop1
 
-inner2_mid:
-    cmp r3, r4
-    blt inner2_loop
 
-inner_done:
+    
+
+loop0_end:
+    ldr r0, addressOfNewline
+    bl printf
     ldr r1, addressOfI
     ldr r2, [r1]
     add r2, r2, #1
     str r2, [r1]
-    b outer_loop
+    b loop0    
 
-outer_mid:
-    cmp r3, r4
-    blt outer_loop
-
-outer_done:
-
-
-// ------------------------------------------------------
-// create the base 
-base:
-    ldr r0, addressOfNewline
-    bl printf
-
-
-    ldr r1, addressOfI          // at this point i has a value of 8, so we need to reset
-    ldr r2, addressOfBaseLength // load up values of rows
-    ldr r3, [r1]                // value in r1 is 8, we will load it into r3
-    mov r4, #0                  // mov a 0 into r4, this is what we will replace the 8 with
-    str r4, [r1]                // store 0 back into memory for the value of r1
-    ldr r4, [r2]                // replace r4 with the value in r2
-    mov r4, r4, lsl #1          // doubling 8 is 16 meaning we can bitshift to the left 1 time
-    sub r4, r4, #1              // subtract 1 to meet conditional we want for the base
-    b base_mid
-
-base_loop:
-    ldr r0, addressOfStars
-    bl printf
-
-    ldr r1, addressOfI
-    ldr r3, [r1]
-    add r3, r3, #1
-    str r3, [r1]
-
-base_mid:
-    cmp r3, r4
-    blt base_loop
-
-
-end:
-    ldr r0, addressOfNewline    // last new line   
-    bl printf
+done:
     ldr lr, [sp], #+4           // pop the lr from the stack
     bx lr
 
@@ -122,10 +73,8 @@ end:
 
 addressOfI: .word i
 addressOfJ: .word j
-addressOfOuterLength: .word outer_length
-addressOfInner1Length: .word inner1_length
-addressOfInner2Length: .word inner2_length
-addressOfBaseLength: .word base_length
+addressOfSpace: .word space
+addressOfRows: .word rows
 addressOfStars: .word stars
 addressOfSpaces: .word spaces
 addressOfNewline: .word newline
